f7b6cbb83acc0cf0c787bb49a7fbd2b40de47281
Ishank Khurana <ishank.khurana@incedoinc.com>
2022-02-10 01:37:07 +0530

    Db function update

---
+++ DBScripts/getClientAccountDetails.sql
@@ -0,0 +1,19 @@
+Alter FUNCTION getAccountInformationForClient (@ClientId Varchar(255))  
+RETURNS TABLE  
+AS  
+RETURN  
+   SELECT 
+   Id,
+   AccountId,  
+CustodianId,  
+CustodianName,  
+RegisteredName,  
+ClientID,  
+CustodianAccountNumber,  
+MarketValue,  
+ProgramId,  
+ProgramName,  
+IsClosed  
+FROM ClientAccount_Detail where ClientID=@ClientId  
+
+ insert into ClientAccount_Detail values('1234','3456','alpha','beta','CL1234','123456','32','12','ishankp',0)
