a33f434273adb7d64f4c54b63475bbdeb483f945
Ishank Khurana <ishank.khurana@incedoinc.com>
2022-02-10 14:41:50 +0530

    changes updated

--- Identity_Service_API/Controllers/Login/ValidationController.cs
+++ Identity_Service_API/Controllers/Login/ValidationController.cs
@@ -22,22 +22,22 @@ namespace Identity_Service_API.Controllers.Login
             {
                 return BadRequest();
             }
-            var Role = await loginRepository.IsAuthenticated(auth.Username, auth.Password);
-            var getRole = Role.FirstOrDefault();
-            if (getRole!= null)
+            var authVal = await loginRepository.IsAuthenticated(auth.Username, auth.Password);
+            var roleValue = authVal.FirstOrDefault();
+            if (roleValue != null)
             {
-                if ((getRole.isvalid == 1) && (getRole.roletype == "Agent" || getRole.roletype == "Admin"))
+                if (roleValue.isvalid == 1 && (roleValue.roletype == "Agent" || roleValue.roletype == "Admin"))
                 {
                     var user = await loginRepository.ValidateLoginDetails(auth.Username, auth.Password);
-                    if (user == null)
+                    if (user.Count <= 0)
                         return NotFound();
                     else
                         return Ok(user);
                 }
-                else if (getRole.isvalid == 1 && getRole.roletype == "Client")
+                else if (roleValue.isvalid == 1 && roleValue.roletype == "Client")
                 {
                     var user = await loginRepository.ValidateclientResponses(auth.Username, auth.Password);
-                    if (user == null)
+                    if (user.Count <= 0)
                         return NotFound();
                     else
                         return Ok(user);
